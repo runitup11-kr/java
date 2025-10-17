b470ceb6f87ebbfc967021222cc8b63d485b88f4
keepgoingautumn <lms301301@naver.com>
2025-10-17 08:19:30 +0000

    주석 코드 설명

--- 각도기Ex.java
+++ 각도기Ex.java
@@ -5,7 +5,9 @@ package testproject;
 
 class Gakdogi {
 	public int gakdogi(int angle) {
+        // 최종적으로 반환할 정답(각의 종류를 나타내는 숫자)을 저장할 변수 (초기값 0
 		int answer = 0;
+        // 각의 종류(예각, 직각, 둔각, 평각)를 문자열로 저장할 변수
 		String gak = null;
 		if(0 < angle && angle < 90) {
 			gak = "예각";
@@ -19,6 +21,7 @@ class Gakdogi {
 		if (angle == 180) {
 			gak = "평각";
 		};
+        //각의 종류(gak) 문자열에 따라 'answer' 변수에 해당하는 정수 값을 설정합니다.
 		switch(gak) {
 		case "예각":
 			answer = 1;
--- 성적구하기Ex.java
+++ 성적구하기Ex.java
@@ -9,9 +9,11 @@ class MyPerson extends Object {
 	int score2;
 	int score3;
 
-	int e = 0;
+	int e = 0; // 성적의 평균을 저장할 변수 (초기값 0)
 	
-	
+
+	// 생성자: MyPerson 객체를 만들 때 초기 데이터를 설정합니다.
+	// n: 이름, a: 나이, s1, s2, s3: 세 과목 점수
 	public MyPerson(String n, int a , int s1, int s2, int s3) {
 		name = n;
 		age = a;
@@ -21,7 +23,7 @@ class MyPerson extends Object {
 		
 		
 	}
-	
+	// 메서드: 세 과목 점수의 평균을 계산하여 'e' 변수에 저장합니다.
 	public void 성적평균구하기() {
 		int result = score1 + score2 + score3;
 		e = result / 3;
@@ -29,9 +31,24 @@ class MyPerson extends Object {
 		
 	}
 	
+	
 	public void 성적표출력() {
 		System.out.println("이름" + name);
 		System.out.println("나이" + age);
 		System.out.println("평균" + e);
 		
 	}
+	
+}
+
+
+public class 성적구하기2Ex extends Object{
+	public static void main(String[] args) {
+        //MyPerson 클래스 객체 (인스턴스) m1 생성.
+		MyPerson m1 = new MyPerson("ㄴㄴㄴ" ,20 ,30 ,40 ,60);
+		m1.성적평균구하기(); //출력
+		m1.성적표출력(); //출력
+
+	}
+
+}
